980b342626edfe601bb08ff38cae1ec6c63cb6d4
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-01 01:39:28 +0530

    sacve

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
+console.log("SERVER FILE IS RUNNING");
+
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("SkillVerse Running 🚀");
+  res.send("SkillVerse Backend is Running 🚀");
 });
 
 app.listen(5000, () => {
-  console.log("Server started on port 5000");
+  console.log("SERVER STARTED ON PORT 5000");
 });
