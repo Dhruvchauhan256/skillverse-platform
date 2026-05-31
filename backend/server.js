09cf564fa0edbc40d07fb4eb8daa159c84cbf7c3
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-01 01:30:56 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
+console.log("STEP 1: FILE LOADED");
+
 const express = require("express");
+console.log("STEP 2: EXPRESS LOADED");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("SkillVerse Backend is Running 🚀");
+  res.send("SkillVerse Running");
 });
 
 app.listen(5000, () => {
-  console.log("Server started on http://localhost:5000");
+  console.log("STEP 3: SERVER STARTED");
 });
