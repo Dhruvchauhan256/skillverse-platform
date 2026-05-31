09499b0bfead602e77b7d0e1c4def0f8a24167a6
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-01 02:32:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "NEW SKILLVERSE API WORKING 🚀"
-  });
+  res.send("🔥 NEW SERVER IS RUNNING (UPDATED CODE)");
 });
 
 app.listen(5000, () => {
-  console.log("SERVER RUNNING ON 5000");
+  console.log("SERVER STARTED FRESH");
 });
