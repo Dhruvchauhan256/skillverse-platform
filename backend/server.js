cc434136c875efe3c0954ad2f74107da1dc2d01a
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-01 02:29:59 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "SkillVerse API Running 🚀"
+    message: "NEW SKILLVERSE API WORKING 🚀"
   });
 });
-const PORT = 5000;
 
-// FORCE IPv4 binding
-app.listen(PORT, "127.0.0.1", () => {
-  console.log("Server running on http://127.0.0.1:" + PORT);
+app.listen(5000, () => {
+  console.log("SERVER RUNNING ON 5000");
 });
