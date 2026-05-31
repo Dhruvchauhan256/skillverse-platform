150388eb12a9c86875cbfff58347c609d93d8f97
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-01 02:04:18 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Test route
+// Home route
 app.get("/", (req, res) => {
   res.json({
+    success: true,
     message: "SkillVerse API Running 🚀"
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log("SkillVerse backend running on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
