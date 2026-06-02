238e3cd52401193663a325c5dac6b2efbaf80e30
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-02 17:43:28 +0530

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Stats from "./components/Stats";
+import Categories from "./components/Categories";
 import Footer from "./components/Footer";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <Stats />
+      <Categories />
       <Footer />
     </>
   );
