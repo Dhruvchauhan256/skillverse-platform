a9eb85a114bc45f25df27f610a44e210d0eebfd4
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-02 17:29:04 +0530

    save

---
+++ frontend/src/components/Stats.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Stats() {
+  return (
+    <section className="stats">
+      <div>
+        <h2>10K+</h2>
+        <p>Freelancers</p>
+      </div>
+
+      <div>
+        <h2>5K+</h2>
+        <p>Projects Completed</p>
+      </div>
+
+      <div>
+        <h2>100+</h2>
+        <p>Categories</p>
+      </div>
+    </section>
+  );
+}
+
+export default Stats;
