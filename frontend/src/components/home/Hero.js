bf28892b7849cba7cb5bc5edd17132a7bab80a38
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-03 02:15:47 +0530

    sdsd

--- frontend/src/components/home/Hero.js
+++ frontend/src/components/home/Hero.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD:frontend/src/components/Hero.js
 import React from "react";
 import "../../styles/hero.css";
 
 function Hero() {
   return (
     <section className="hero-section">
-
       <div className="hero-content">
         <h1>
           Hire Top Freelancers <br />
@@ -22,40 +20,8 @@ function Hero() {
           <button className="btn-secondary">Find Work</button>
         </div>
       </div>
-
     </section>
   );
 }
 
 export default Hero;
-=======
-import React from "react";
-import "../styles/hero.css";
-
-function Hero() {
-  return (
-    <section className="hero-section">
-
-      <div className="hero-content">
-        <h1>
-          Hire Top Freelancers <br />
-          & Grow Your Business 🚀
-        </h1>
-
-        <p>
-          SkillVerse connects clients with top skilled developers, designers,
-          and digital experts worldwide.
-        </p>
-
-        <div className="hero-buttons">
-          <button className="btn-primary">Hire Talent</button>
-          <button className="btn-secondary">Find Work</button>
-        </div>
-      </div>
-
-    </section>
-  );
-}
-
-export default Hero;
->>>>>>> 1da308d (save work before pull):frontend/src/components/home/Hero.js
