761923eeaf2d27ee12ce53f98856744476fba342
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-03 00:51:41 +0530

    sdvd

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -6,19 +6,44 @@ function Register() {
   return (
     <div className="auth-container">
       <div className="auth-card">
-        <h2>Signup</h2>
 
-        <form>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+        <h2 className="auth-title">Create Account 🚀</h2>
+        <p className="auth-subtitle">
+          Join <span>SkillVerse</span> and start earning
+        </p>
 
-          <button type="submit">Signup</button>
+        <form className="auth-form">
+          <input
+            type="text"
+            placeholder="Enter Name"
+            className="auth-input"
+            required
+          />
+
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="auth-input"
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="auth-input"
+            required
+          />
+
+          <button type="submit" className="auth-btn">
+            Signup
+          </button>
         </form>
 
-        <p>
-          Already have an account? <Link to="/login">Login</Link>
+        <p className="auth-footer">
+          Already have an account?{" "}
+          <Link to="/login">Login</Link>
         </p>
+
       </div>
     </div>
   );
