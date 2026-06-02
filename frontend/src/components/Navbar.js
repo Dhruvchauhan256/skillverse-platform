78a375f104342feb171d9fe9c20793deadf89612
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-02 23:38:35 +0530

    save

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,10 +6,12 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
       <div className="container">
 
-        <a className="navbar-brand fw-bold fs-3 text-info" href="/">
+        {/* Brand */}
+        <Link className="navbar-brand fw-bold fs-3 text-info" to="/">
           SkillVerse
-        </a>
+        </Link>
 
+        {/* Mobile Toggle */}
         <button
           className="navbar-toggler"
           type="button"
@@ -19,29 +21,38 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
+        {/* Navbar Links */}
         <div className="collapse navbar-collapse" id="navbarNav">
 
           <ul className="navbar-nav ms-auto align-items-center">
 
             <li className="nav-item">
-              <Link className="nav-link" to="/find-work">Find Work</Link>
+              <Link className="nav-link" to="/find-work">
+                Find Work
+              </Link>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="/">Find Talent</a>
+              <Link className="nav-link" to="/find-talent">
+                Find Talent
+              </Link>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="/">Categories</a>
+              <Link className="nav-link" to="/categories">
+                Categories
+              </Link>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="/">Login</a>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
             </li>
 
             <li className="nav-item ms-2">
               <Link to="/signup" className="btn btn-info fw-bold">
-                  Join Now
+                Join Now
               </Link>
             </li>
 
