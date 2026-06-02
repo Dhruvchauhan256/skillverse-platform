f46b21bcb633ad5aef5a337e12da5e24c0a8548f
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-03 02:16:18 +0530

    save

--- frontend/src/components/home/Categories.js
+++ frontend/src/components/home/Categories.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD:frontend/src/components/Categories.js
-import React from "react";
-import CategoryCard from "../CategoryCard";
-
-function Categories() {
-  const categories = [
-    "Web Development",
-    "UI/UX Design",
-    "AI & Machine Learning",
-    "Mobile App Development",
-    "Digital Marketing",
-    "SEO",
-    "Content Writing",
-    "Graphic Design"
-  ];
-
-  return (
-    <section className="categories">
-      <h2>Popular Categories</h2>
-
-      <div className="grid">
-        {categories.map((category, index) => (
-          <CategoryCard key={index} title={category} />
-        ))}
-      </div>
-    </section>
-  );
-}
-
-export default Categories;
-=======
 import React from "react";
 import CategoryCard from "./CategoryCard";
 
@@ -58,4 +27,3 @@ function Categories() {
 }
 
 export default Categories;
->>>>>>> 1da308d (save work before pull):frontend/src/components/home/Categories.js
