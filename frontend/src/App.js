4262020041bad2ee3d34aa078b777ccdc8457f6a
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-02 16:53:35 +0530

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="container text-center mt-5">
-      <h1 className="display-3 fw-bold">🚀 SkillVerse</h1>
-
-      <p className="lead">
-        India's Smart Freelance Marketplace
-      </p>
-
-      <div className="mt-4">
-        <button className="btn btn-primary me-3">
-          Hire Freelancer
-        </button>
-
-        <button className="btn btn-success">
-          Become Freelancer
-        </button>
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
   );
 }
 
