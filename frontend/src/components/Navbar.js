494842ca312153a8c814a47b64fcea6dc3c71c4e
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-02 16:49:34 +0530

    save

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,65 +1,50 @@
 import React from "react";
 
 function Navbar() {
-return ( <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> <div className="container">
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="/">
+          SkillVerse
+        </a>
 
-```
-    <a className="navbar-brand fw-bold" href="/">
-      SkillVerse
-    </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home</a>
+            </li>
 
-    <div className="collapse navbar-collapse" id="navbarNav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Find Work</a>
+            </li>
 
-      <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Find Talent</a>
+            </li>
 
-        <li className="nav-item">
-          <a className="nav-link" href="/">
-            Home
-          </a>
-        </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Login</a>
+            </li>
 
-        <li className="nav-item">
-          <a className="nav-link" href="/">
-            Find Work
-          </a>
-        </li>
-
-        <li className="nav-item">
-          <a className="nav-link" href="/">
-            Find Talent
-          </a>
-        </li>
-
-        <li className="nav-item">
-          <a className="nav-link" href="/">
-            Login
-          </a>
-        </li>
-
-        <li className="nav-item">
-          <a className="btn btn-primary ms-2" href="/">
-            Sign Up
-          </a>
-        </li>
-
-      </ul>
-
-    </div>
-
-  </div>
-</nav>
-```
-
-);
+            <li className="nav-item">
+              <a className="btn btn-primary ms-2" href="/">
+                Sign Up
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
