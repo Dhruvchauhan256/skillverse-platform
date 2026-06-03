51b57b31d045bb4419e937011950977565a38efb
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-03 13:50:42 +0530

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import FreelancerDashboard from "./pages/dashboard/FreelancerDashboard";
 import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
