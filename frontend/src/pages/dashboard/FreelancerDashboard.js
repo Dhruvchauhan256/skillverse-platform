52045a934952cf44cd635969d3678cb2c99b5f78
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-03 17:22:46 +0530

    save

---
+++ frontend/src/pages/dashboard/FreelancerDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FreelancerDashboard() {
+  return (
+    <div className="container mt-5">
+      <h1>Freelancer Dashboard</h1>
+      <p>Welcome to SkillVerse Dashboard.</p>
+    </div>
+  );
+}
+
+export default FreelancerDashboard;
