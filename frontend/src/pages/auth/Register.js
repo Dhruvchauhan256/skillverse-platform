c734cc346db7ac766f61cf1afc87bed86ba72bdc
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-06 17:53:58 +0530

    save

--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Auth.css";
 
 function Register() {
