9743c7bb1a15b0f69e943310fb489345d4129031
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-06 17:15:05 +0530

    save

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,68 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
-      <div className="container">
+  const token = localStorage.getItem("token");
 
-        {/* Brand */}
-        <Link className="navbar-brand fw-bold fs-3 text-info" to="/">
+  return (
+    <nav className="custom-navbar">
+      <div className="nav-container">
+
+        {/* BRAND */}
+        <Link className="brand" to="/">
           SkillVerse
         </Link>
 
-        {/* Mobile Toggle */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        {/* LINKS */}
+        <div className="nav-links">
 
-        {/* Navbar Links */}
-        <div className="collapse navbar-collapse" id="navbarNav">
+          <Link to="/find-work">Find Work</Link>
+          <Link to="/find-talent">Find Talent</Link>
+          <Link to="/jobs">Jobs</Link>
 
-          <ul className="navbar-nav ms-auto align-items-center">
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/find-work">
-                Find Work
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/find-talent">
-                Find Talent
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/search">
-                Categories
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-
-            <li className="nav-item ms-2">
-              <Link to="/signup" className="btn btn-info fw-bold">
-                Join Now
-              </Link>
-            </li>
-
-          <li className="nav-item ms-2">
-              <Link className="nav-link" to="/dashboard">
-                    Dashboard
-              </Link>
-          </li>
-
-          </ul>
+          {!token ? (
+            <>
+              <Link className="login-btn" to="/login">Login</Link>
+              <Link className="signup-btn" to="/signup">Join</Link>
+            </>
+          ) : (
+            <Link className="dashboard-btn" to="/dashboard">
+              Dashboard
+            </Link>
+          )}
 
         </div>
 
