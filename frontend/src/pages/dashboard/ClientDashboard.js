c911598f203335a2cd17eb8c0fe0d722144f50b3
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-07 23:44:56 +0530

    sdsd

---
+++ frontend/src/pages/dashboard/ClientDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ClientDashboard() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Client Dashboard</h1>
+      <p>SkillVerse Client Dashboard Working ✅</p>
+    </div>
+  );
+}
+
+export default ClientDashboard;
