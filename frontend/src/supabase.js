992dacfd85d3688f2440cbfd1641efebef0a0c13
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-07 19:56:36 +0530

    save

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "postgresql://postgres:[YOUR-PASSWORD]@db.waljvgvdbxqzfzlmqwtk.supabase.co:5432/postgres";
+const supabaseUrl = "postgresql://postgres:SkillVerse@2026@db.waljvgvdbxqzfzlmqwtk.supabase.co:5432/postgres";
 const supabaseKey = "sb_publishable_BRjF2nzuv71somtADpNGXg_nCSwDYpB";
 
 export const supabase = createClient(
