c9398f0edd9cb751ccbc26a44830b85901e6f36d
dhruv <dhruvharishchauhan@gmail.com>
2026-06-08 22:52:32 +0530

    Message

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,34 +1,37 @@
+const jwt = require("jsonwebtoken");
 
-DHRUV HARISH CHAUHAN@LAPTOP-AAUELRQ1 MINGW64 ~/skillverse-platform/backend (main)
-$ cat middleware/authMiddleware.js
 const protect = (req, res, next) => {
-  console.log("AUTH HEADER:", req.headers.authorization);
+console.log("AUTH HEADER:", req.headers.authorization);
 
-  let token = req.headers.authorization;
+let token = req.headers.authorization;
 
-  if (!token) {
-    return res.status(401).json({
-      success: false,
-      message: "No token provided",
-    });
-  }
+if (!token) {
+return res.status(401).json({
+success: false,
+message: "No token provided",
+});
+}
 
-  try {
-    token = token.split(" ")[1];
+try {
+token = token.split(" ")[1];
 
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET
-    );
+```
+const decoded = jwt.verify(
+  token,
+  process.env.JWT_SECRET
+);
 
-    req.user = decoded;
+req.user = decoded;
 
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      success: false,
-      message: "Invalid token",
-    });
-  }
+next();
+```
+
+} catch (error) {
+return res.status(401).json({
+success: false,
+message: "Invalid token",
+});
+}
 };
-DHRUV HARISH CHAUHAN@LAPTOP-AAUELRQ1 MINGW64 ~/skillverse-platform/backend (main)
+
+module.exports = protect;
