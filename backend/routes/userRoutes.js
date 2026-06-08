1a2c12617bf708ad2d50de39064dacfa9fe89ec7
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-09 03:45:42 +0530

    save

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,29 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const protect = require("../middleware/authMiddleware");
+console.log("✅ userRoutes.js loaded");
 
-const {
-  getCurrentUser,
-  updateFreelancerProfile,
-} = require("../controllers/userController");
-
-// TEST ROUTE
 router.get("/test", (req, res) => {
   res.status(200).json({
     success: true,
     message: "User Routes Working ✅",
   });
 });
-
-// GET CURRENT USER
-router.get("/me", protect, getCurrentUser);
-
-// UPDATE FREELANCER PROFILE
-router.put(
-  "/freelancer/profile",
-  protect,
-  updateFreelancerProfile
-);
 
 module.exports = router;
