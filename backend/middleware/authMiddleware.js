74d33f3af9020b3c25cda9fd3a223383b0b3a51d
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-09 03:53:14 +0530

    save

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,37 +1,38 @@
 const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
-console.log("AUTH HEADER:", req.headers.authorization);
+  console.log("AUTH HEADER:", req.headers.authorization);
 
-let token = req.headers.authorization;
+  let token = req.headers.authorization;
 
-if (!token) {
-return res.status(401).json({
-success: false,
-message: "No token provided",
-});
-}
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "No token provided",
+    });
+  }
 
-try {
-token = token.split(" ")[1];
+  try {
+    token = token.split(" ")[1];
 
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
 
-const decoded = jwt.verify(
-  token,
-  process.env.JWT_SECRET
-);
+    console.log("DECODED USER:", decoded);
 
-req.user = decoded;
+    req.user = decoded;
 
-next();
+    next();
+  } catch (error) {
+    console.log(error);
 
-
-} catch (error) {
-return res.status(401).json({
-success: false,
-message: "Invalid token",
-});
-}
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
 };
 
 module.exports = protect;
