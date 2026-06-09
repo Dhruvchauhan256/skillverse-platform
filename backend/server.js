36ae8800713ff3bac07067b443dd596536c5c654
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-09 19:08:41 +0530

    dsd

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,40 @@ const app = express();
 // MIDDLEWARE (IMPORTANT ORDER)
 app.use(cors());
 app.use(express.json());
+exports.createGig = async (req, res) => {
+  try {
+    const { title, description, price, category } = req.body;
 
+    if (!title || !description || !price || !category) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const gig = await prisma.gig.create({
+      data: {
+        title,
+        description,
+        price,
+        category,
+        freelancerId: req.user.id,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      gig,
+    });
+
+  } catch (error) {
+    console.log("GIG ERROR:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
 // ROUTES
 const authRoutes = require("./routes/authRoutes");
 const gigRoutes = require("./routes/gigRoutes");
