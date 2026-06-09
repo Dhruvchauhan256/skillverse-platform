4d5f889a250ee59312f8235efe9d7802bd97efa4
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-09 19:48:50 +0530

    swdw

--- backend/routes/gigRoutes.js
+++ backend/routes/gigRoutes.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const protect = require("../middleware/authMiddleware");
-const gigController = require("../controllers/gigController");
+const {
+  createGig,
+  getAllGigs,
+} = require("../controllers/gigController");
 
-// DEBUG CHECK (important)
-console.log("gigController:", gigController);
-
-// ROUTES
-router.post("/", protect, gigController.createGig);
-router.get("/", gigController.getAllGigs);
+router.post("/", protect, createGig);
+router.get("/", getAllGigs);
 
 module.exports = router;
