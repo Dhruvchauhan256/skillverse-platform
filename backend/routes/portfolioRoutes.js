9c35dd1d91275fa7c1632283e727f223e78aeb7a
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-09 18:44:42 +0530

    dswd

--- backend/routes/portfolioRoutes.js
+++ backend/routes/portfolioRoutes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const protect = require("../middleware/authMiddleware");
+
 const {
   createPortfolio,
   getMyPortfolio,
-  deletePortfolio,
   updatePortfolio,
+  deletePortfolio,
 } = require("../controllers/portfolioController");
 
-// MUST USE protect middleware
 router.post("/", protect, createPortfolio);
 router.get("/", protect, getMyPortfolio);
 router.put("/:id", protect, updatePortfolio);
