4902ba1c36aafe4b642f9a527fa15ea7bacf32d0
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-14 02:26:34 +0530

    save

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -10,7 +10,20 @@ getMyProjects,
 } = require("../controllers/projectController");
 
 router.get("/", getAllProjects);
+router.get("/debug-users", async (req, res) => {
+  const prisma = require("../prisma/client");
 
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+    },
+  });
+
+  res.json(users);
+});
 router.get("/my", protect, getMyProjects);
 
 router.post("/", protect, createProject);
