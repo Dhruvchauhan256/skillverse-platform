00790d150aafe37bafeedd6b3be56f23f0473f85
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-14 03:42:14 +0530

    save

---
+++ frontend/src/pages/client/ClientProfile.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ClientProfile() {
+  return (
+    <div className="container mt-5">
+      <h2>Client Profile</h2>
+
+      <div className="card p-4 shadow-sm">
+        <h4>Demo Client</h4>
+
+        <p>Email: client@skillverse.com</p>
+
+        <p>
+          Company: SkillVerse Technologies
+        </p>
+
+        <p>
+          Posted Projects: 10
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ClientProfile;
