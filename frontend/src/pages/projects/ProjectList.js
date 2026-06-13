cde0d59730db1655b97bdc50ea040e6af4122327
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-14 03:42:47 +0530

    sacve

---
+++ frontend/src/pages/projects/ProjectList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProjectList() {
+  return (
+    <div className="container mt-5">
+      <h2>Project List</h2>
+
+      <div className="alert alert-success">
+        Project List Page Working
+      </div>
+    </div>
+  );
+}
+
+export default ProjectList;
