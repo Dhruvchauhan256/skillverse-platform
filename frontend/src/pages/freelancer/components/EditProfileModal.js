512886fce40271fa03f7a3fef89790c37b5e5feb
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-14 15:22:13 +0530

    dwfwe

---
+++ frontend/src/pages/freelancer/components/EditProfileModal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function EditProfileModal() {
+  return (
+    <div className="card p-3">
+      <h5>Edit Profile (Coming Soon)</h5>
+      <p>This will be connected to backend later.</p>
+    </div>
+  );
+}
+
+export default EditProfileModal;
