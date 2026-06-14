326b187b26c6cc5e09e259626a18da80e762d40c
dhruv <dhruvharishchauhan@gmail.com>
2026-06-14 18:58:50 +0530

    Message

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -3,10 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-console.log("SUPABASE URL:", supabaseUrl);
-console.log("SUPABASE KEY:", supabaseAnonKey);
-
 if (!supabaseUrl || !supabaseAnonKey) {
+  console.log("ENV CHECK:", {
+    supabaseUrl,
+    supabaseAnonKey,
+  });
+
   throw new Error("Missing Supabase environment variables");
 }
 
