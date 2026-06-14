28f0e42227f4bc1a1c86c5f5d7c326effa071eab
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-14 15:21:54 +0530

    dwfe

---
+++ frontend/src/pages/freelancer/components/PortfolioSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PortfolioSection() {
+  return (
+    <div className="card p-3 mb-3">
+      <h5>Portfolio</h5>
+      <p>Showcase your best work here...</p>
+    </div>
+  );
+}
+
+export default PortfolioSection;
