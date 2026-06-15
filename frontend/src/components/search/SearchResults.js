529ff9006934d0aff6e557ea5d7c2ef1c839cc36
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-16 00:40:16 +0530

    sav

--- frontend/src/components/search/SearchResults.js
+++ frontend/src/components/search/SearchResults.js
@@ -1,5 +1,5 @@
 import React from "react";
-import FreelancerCard from "./FreelancerCard";
+import FreelancerCard from "../freelancer/FreelancerCard";
 
 function SearchResults({ data, query }) {
   if (!query) return null;
