9a8bc1ccfafd9d652d19f638e1d13b02d825617d
Dhruvchauhan256 <149219182+Dhruvchauhan256@users.noreply.github.com>
2026-06-24 14:57:37 +0530

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+
 // Components
 import Navbar from "./components/common/Navbar.jsx";
 import MobileNav from "./components/common/MobileNav.jsx";
@@ -41,139 +41,140 @@ import FreelancerSearch from "./pages/freelancer/FreelancerSearch";
 import CategorySearch from "./pages/search/CategorySearch";
 
 function App() {
- return (
+  return (
     <AuthProvider>
       <>
-      {/* TOP NAVBAR */}
-      <Navbar />
+        {/* TOP NAVBAR */}
+        <Navbar />
 
-      {/* ROUTES ONLY */}
-      <Routes>
+        {/* ROUTES ONLY */}
+        <Routes>
 
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
-<Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/find-work" element={<FindWork />} />
-        <Route path="/find-talent" element={<FindTalent />} />
-        <Route path="/freelancers" element={<FreelancerSearch />} />
-        <Route path="/search" element={<CategorySearch />} />
-        <Route path="/jobs" element={<JobsPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/onboarding" element={<Onboarding />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <FreelancerDashboard />
-            </ProtectedRoute>
-          }
-        />
+          <Route path="/find-work" element={<FindWork />} />
+          <Route path="/find-talent" element={<FindTalent />} />
+          <Route path="/freelancers" element={<FreelancerSearch />} />
+          <Route path="/search" element={<CategorySearch />} />
+          <Route path="/jobs" element={<JobsPage />} />
 
-        <Route
-          path="/client-dashboard"
-          element={
-            <ProtectedRoute>
-              <ClientDashboard />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <FreelancerDashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/client-profile"
-          element={
-            <ProtectedRoute>
-              <ClientProfile />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/client-dashboard"
+            element={
+              <ProtectedRoute>
+                <ClientDashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/post-project"
-          element={
-            <ProtectedRoute>
-              <PostProject />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/client-profile"
+            element={
+              <ProtectedRoute>
+                <ClientProfile />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/projects"
-          element={
-            <ProtectedRoute>
-              <ProjectList />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/post-project"
+            element={
+              <ProtectedRoute>
+                <PostProject />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/proposal"
-          element={
-            <ProtectedRoute>
-              <SubmitProposal />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/projects"
+            element={
+              <ProtectedRoute>
+                <ProjectList />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/my-proposals"
-          element={
-            <ProtectedRoute>
-              <MyProposals />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/proposal"
+            element={
+              <ProtectedRoute>
+                <SubmitProposal />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/proposal-management"
-          element={
-            <ProtectedRoute>
-              <ProposalManagement />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/my-proposals"
+            element={
+              <ProtectedRoute>
+                <MyProposals />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/messages"
-          element={
-            <ProtectedRoute>
-              <Messages />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/proposal-management"
+            element={
+              <ProtectedRoute>
+                <ProposalManagement />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/freelancer/:name"
-          element={<FreelancerProfile />}
-        />
+          <Route
+            path="/messages"
+            element={
+              <ProtectedRoute>
+                <Messages />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/freelancer-profile"
-          element={<FreelancerProfile />}
-        />
+          <Route
+            path="/freelancer/:name"
+            element={<FreelancerProfile />}
+          />
 
-        <Route
-          path="/my-projects"
-          element={
-            <ProtectedRoute>
-              <MyProjects />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/freelancer-profile"
+            element={<FreelancerProfile />}
+          />
 
-        <Route
-          path="/edit-project/:id"
-          element={
-            <ProtectedRoute>
-              <EditProject />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/my-projects"
+            element={
+              <ProtectedRoute>
+                <MyProjects />
+              </ProtectedRoute>
+            }
+          />
 
-      </Routes>
+          <Route
+            path="/edit-project/:id"
+            element={
+              <ProtectedRoute>
+                <EditProject />
+              </ProtectedRoute>
+            }
+          />
 
-      {/* MOBILE NAV (IMPORTANT: OUTSIDE ROUTES) */}
-    <MobileNav />
+        </Routes>
+
+        {/* MOBILE NAV (IMPORTANT: OUTSIDE ROUTES) */}
+        <MobileNav />
       </>
     </AuthProvider>
   );
